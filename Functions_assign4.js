22394732b0afdab6c6be513b3c60334c84b3b275
G-wize <3099913@gscs.ca>
2026-09-18 09:41:21 -0600

    Add files via upload

---
+++ Functions_assign4.js
@@ -0,0 +1,44 @@
+/* Gabriel,
+3099913@gscs.ca, 
+Functions and Abstraction-assignment 4
+*/
+
+function setup() {
+  createCanvas(500,500) // extends canvas to 500 by 500
+  
+  //function to draw a 50x50 squaree
+  function draw_fixed_square()
+  {
+    //draws 50 by 50 square
+    rect(50,50,50,50);
+    //line(50,53,100,53);
+  }
+  
+  //function to draw a circle with its center at x,y and the width and height at d,d 
+  function draw_circle(x,y,d)
+  {
+    //draws circle 
+    ellipse(x,y,d,d);
+  }
+  
+  //draws 3 circles of varying radii within the same center
+  function draw_concentric_circles(x,y)
+  {
+    //draws circle with raduis 90
+    ellipse(x,y,90,90);
+    //draws circle with raduis 60
+    ellipse(x,y,60,60);
+    //draws circle with raduis 30
+    ellipse(x,y,30,30);
+  }
+  
+  draw_fixed_square();
+  draw_circle(40,40,20);
+  draw_concentric_circles(135,135)
+
+}
+
+
+function draw() {
+
+}
